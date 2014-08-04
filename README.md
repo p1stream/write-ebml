6379ab7287e0bc3042ca61f16c5e56c0a90f35a0
Stéphan Kochen <stephan@kochen.nl>
2014-08-04 21:57:09 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,31 @@
+## Write EBML
+
+A simple EBML writer. Currently targets node.js 0.11.
+
+    var writeEBML = require('write-ebml');
+
+    // Define a tag:
+    var myTag = writeEBML.tag(0x1234, '8');
+
+    // Some standard tags:
+    var T = writeEBML.standardTags;
+
+    // Write a document:
+    var buf = writeEBML([
+        T.EBML([
+            T.EBMLVersion(1),
+            T.EBMLReadVersion(1),
+            T.EBMLMaxIDLength(4),
+            T.EBMLMaxSizeLength(8),
+            T.DocType('demo'),
+            T.DocTypeVersion(1),
+            T.DocTypeReadVersion(1)
+        ]),
+        myTag('Hello world!')
+    ]);
+
+### License
+
+[GPLv3](LICENSE)
+
+© 2014 — Stéphan Kochen
---
+++ index.js
@@ -0,0 +1,29 @@
+exports = module.exports = require('./build/Release/ebml.node');
+
+var tag = exports.tag = function(id, type) {
+    var b = new Buffer(4);
+    b.writeUInt32BE(id, 0);
+    if (id <= 0xFF)
+        b = b.slice(3, 4);
+    else if (id <= 0xFFFF)
+        b = b.slice(2, 4);
+    else if (id <= 0xFFFFFF)
+        b = b.slice(1, 4);
+    else if (id > 0xFFFFFFFF)
+        throw new Error('Invalid ID');
+
+    return function(val, sizeUnknown) {
+        return [b, type, val, sizeUnknown];
+    };
+};
+
+exports.standardTags = {
+    EBML: tag(0x1A45DFA3, 'm'),
+    EBMLVersion: tag(0x4286, 'u'),
+    EBMLReadVersion: tag(0x42F7, 'u'),
+    EBMLMaxIDLength: tag(0x42F2, 'u'),
+    EBMLMaxSizeLength: tag(0x42F3, 'u'),
+    DocType: tag(0x4282, 's'),
+    DocTypeVersion: tag(0x4287, 'u'),
+    DocTypeReadVersion: tag(0x4285, 'u')
+};
